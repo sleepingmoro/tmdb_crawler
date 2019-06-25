4e2f557cf1e5ab005dfa483b77e1f3f5aa170e00
moro <sleeping.moro@gmail.com>
2019-06-26 07:13:45 +0900

    people.age => people.birthday 컬럼 수정

--- db/migrate/20190625185911_create_movies.rb
+++ db/migrate/20190625185911_create_movies.rb
@@ -1,5 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
+    # TODO: not null 수정, tmdb_id unique 속성 추가
     create_table :movies do |t|
       t.string :title
       t.text :desc
--- db/migrate/20190625190032_create_people.rb
+++ db/migrate/20190625190032_create_people.rb
@@ -2,7 +2,7 @@ class CreatePeople < ActiveRecord::Migration[5.2]
   def change
     create_table :people do |t|
       t.string :name
-      t.integer :age
+      t.string :birthday
       t.integer :gender
 
       t.timestamps
