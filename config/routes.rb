006ac30cb90fc76d78044281139375ae0611a4ee
moro <sleeping.moro@gmail.com>
2019-06-26 09:10:33 +0900

    peoplecontroller 생성, people index page 생성

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
+  resources :people, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/test/:id' => 'application#crawl'
 end
